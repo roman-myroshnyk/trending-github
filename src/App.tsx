edf30fe842a56c4f95d74bcf24071fcefd5fcf67
Roman <roman.myroshnyk@gmail.com>
2021-12-15 21:58:34 +0900

    cleaned up

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
+// react
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-
+import { QueryClient, QueryClientProvider } from "react-query";
+// context
+import TrendingContextProvider from "./context/TrendingContext";
 // pages
 import RepositoriesPage from "./pages/TrendingRepositoriesPage/TrendingRepositoriesPage";
 import DevelopersPage from "./pages/TrendingDevelopersPage/TrendingDevelopersPage";
-
 // styles
 import "./App.css";
+
+// react-query client
+const queryClient = new QueryClient();
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
-      <div className="App">
-        <Switch>
-          <Route path="/repositories/:language?">
-            <RepositoriesPage />
-          </Route>
-          <Route path="/developers/:language?">
-            <DevelopersPage />
-          </Route>
-        </Switch>
-      </div>
+      <QueryClientProvider client={queryClient}>
+        <TrendingContextProvider>
+          <main className="App">
+            <Switch>
+              <Route path="/repositories/:language?">
+                <RepositoriesPage />
+              </Route>
+              <Route path="/developers/:language?">
+                <DevelopersPage />
+              </Route>
+            </Switch>
+          </main>
+        </TrendingContextProvider>
+      </QueryClientProvider>
     </BrowserRouter>
   );
 };
